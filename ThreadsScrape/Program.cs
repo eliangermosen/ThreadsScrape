c00bc0419d3a7cb67d8fa19cf85410c104096b3c
elianmtg <elianmtoribio@gmail.com>
2021-11-09 18:54:35 -0400

    complete until future change

--- ThreadsScrape/Program.cs
+++ ThreadsScrape/Program.cs
@@ -7,10 +7,14 @@ namespace ThreadsScrape
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            ScrapingThreads scraping = new ScrapingThreads();//instancia clase de  con hilo
-            Thread itla = new Thread(new ThreadStart(scraping.hiloItla));//hilo 1 de ITLA
-            Thread h2 = new Thread(new ThreadStart(scraping.hiloDos));//hilo 2 de 
+            /*NORMAL*/
+            //Scraping scraping = new Scraping();
+            //scraping.hiloItla();
+            //scraping.hiloDos();
+            /*CON HILO*/
+            ScrapingThreads scrapingThreads = new ScrapingThreads();//instancia clase de  con hilo
+            Thread itla = new Thread(new ThreadStart(scrapingThreads.hiloItla));//hilo 1 de ITLA
+            Thread h2 = new Thread(new ThreadStart(scrapingThreads.hiloDos));//hilo 2 de 
             itla.Start();
             h2.Start();
         }
